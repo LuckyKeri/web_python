10d3f83cd67112ccc43f16774c7a811835b5367e
LuckyKeri <kerimkulovab1997@gmail.com>
2024-01-08 20:19:17 +0900

    created app.py, templates, static

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template 
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/page_2")
+def p_2():
+    return render_template("page_2.html")
+
+@app.route("/page_3")
+def p_3():
+    return render_template("page_3.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
